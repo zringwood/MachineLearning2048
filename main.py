027b481463dd2c835f1de858a7da388a5db46d28
Zachary Ringwood <zringwood@gmail.com>
2022-05-11 16:35:21 -0400

    Add files via upload
    All needed moves are now possible in the game.

--- main.py
+++ main.py
@@ -16,23 +16,70 @@ def newgame() :
     while secondnum == firstnum :
         secondnum = [randint(0,3),randint(0,3)]
     board[secondnum[0]][secondnum[1]] = 2
-#updates the board to the left
-def update(direction) :
+#Helper method, clears the 0s from the board
+def removezeros():
     global board
-    for i in range(len(board)) :
+    for line in board:
+        while line.count(0) != 0:
+            line.remove(0)
+def updateleft():
+    global board
+    #first, we remove all the zeros.
+    removezeros()
+    #this allows us to add adjacent cells without worrying about zeros in between
+    for i in range(len(board)):
         for j in range(len(board[i])-1):
-            if board[i][j] == board[i][j+1] :
-                board[i][j+1] = board[i][j] + board[i][j+1]
-                board[i][j] = 0
-            elif board[i][j] != 0 and board[i][j+1] == 0:
-                board[i][j+1] = board[i][j]
-                board[i][j] = 0
+            if board[i][j] == board[i][j+1]:
+                board[i][j] = board[i][j] + board[i][j+1]
+                board[i].pop(j+1)
+    #then we add back the zeros
+    for line in board:
+        while len(line) < 4 :
+            line.append(0)
+def updateright():
+    global board
+    #first, we remove all the zeros.
+    removezeros()
+    #this allows us to add adjacent cells without worrying about zeros in between
+    for i in range(len(board)):
+        for j in range(len(board[i])-1):
+            if board[i][j] == board[i][j+1]:
+                board[i][j] = board[i][j] + board[i][j+1]
+                board[i].pop(j+1)
+    
+    #then we add back the zeros
+    for i in range(len(board)):
+        while len(board[i]) < 4 :
+            board[i] = [0] + board[i]
 
+def transposeboard():
+    global board
+    newboard = [[0] * 4 for i in range(4)]
+    for i in range(4):
+        for j in range(4):
+            newboard[i][j] = board[j][i]
+    board = newboard
+#to perform updates up or down, we just rotate the board and then update either left or right.
+def updatedown():
+    transposeboard()
+    updateright()
+    transposeboard()
+def updateup():
+    transposeboard()
+    updateleft()
+    transposeboard()
 newgame()
-board[0] = [2,0,4,4]
+board[0] = [2,4,4,0]
+board[1] = [0,4,0,2]
+test = [0,0,0,2]
+##for j in range(len(test)):
+##    for k in range(len(test)-1):
+##        if test[k] == 0 :
+##            save = test[k]
+##            test[k] = test[k+1]
+##            test[k+1] = save
+##print(test)
 printboard()
-
-update("right")
-printboard()
-update("right")
+print()
+updateup()
 printboard()
