e540d1d769729a60fd6e3d13e0f16d54a6f35ec9
Zachary Ringwood <zringwood@gmail.com>
2022-06-09 16:20:50 -0400

    Add files via upload
    Finally implemented NEAT. It doesn't solve the game but it does move the peices around effectively.

--- main.py
+++ main.py
@@ -1,116 +1,64 @@
-from random import *
-#the 2048 board
-board = [[0] * 4 for i in range(4)]
-#method to help debug a little
-def printboard() :
-    for line in board:
-        print(line)
-#Adds the next new tile to the board.
-def addnewtile():
-    global board
-    #Iterate over the board, add all zeros to an array, then pick two randomly.
-    zeroes = []
-    for i in range(len(board)) :
-        for j in range(len(board)) :
-            if board[i][j] == 0:
-                zeroes.append([i,j])
-    #We now have an array of all zero coordinates on the board.
-    index = randint(0,len(zeroes)-1)
-    board[zeroes[index][0]][zeroes[index][1]] = 2
-#sets everything to none except two cells, which are either 2 or 4
-def newgame() :
-    global board
-    board = [[0] * 4 for i in range(4)]
-    addnewtile()
-    addnewtile()
-#Helper method, clears the 0s from the board
-def removezeros(currboard):
-    board = [row[:] for row in currboard]
-    for line in board:
-        while line.count(0) != 0:
-            line.remove(0)
-    return board
-def updateleft(currboard):
-    board = [row[:] for row in currboard]
-    #first, we remove all the zeros.
-    removezeros(board)
-    #this allows us to add adjacent cells without worrying about zeros in between
-    for i in range(len(board)):
-        for j in range(len(board[i])-1):
-            #Because board[i] is changing size in this loop we need to check j
-            if j < len(board[i])-1 and board[i][j] == board[i][j+1]:
-                board[i][j] = board[i][j] + board[i][j+1]
-                board[i].pop(j+1)
-                
-    #then we add back the zeros
-    for line in board:
-        while len(line) < 4 :
-            line.append(0)
-    return board
-def updateright(currboard):
-    board = [row[:] for row in currboard]
-    #first, we remove all the zeros.
-    removezeros(board)
-    #this allows us to add adjacent cells without worrying about zeros in between
-    for i in range(len(board)):
-        for j in range(len(board[i])-1):
-            if j < len(board[i])-1 and board[i][j] == board[i][j+1]:
-                board[i][j] = board[i][j] + board[i][j+1]
-                board[i].pop(j+1)
-    
-    #then we add back the zeros
-    for i in range(len(board)):
-        while len(board[i]) < 4 :
-            board[i] = [0] + board[i]
-    return board
-def transposeboard(currboard):
-    newboard = [[0] * 4 for i in range(4)]
-    for i in range(4):
-        for j in range(4):
-            newboard[i][j] = currboard[j][i]
-    return newboard
-    
-#to perform updates up or down, we just transpose the board and then update either left or right.
-def updatedown(currboard):
-    board = transposeboard(currboard)
-    board = updateright(board)
-    return transposeboard(board)
-def updateup(currboard):
-    board = transposeboard(currboard)
-    board = updateleft(board)
-    return transposeboard(currboard)
-def haspossiblemoves(board):
-    for i in range(len(board)):
-        for j in range(len(board[i])):
-            #It's possible to do these three if statements in one like but it looks very awkward.
-            if board[i][j] == 0 :
-                return True
-            if j < len(board[i])-1 and board[i][j] == board[i][j+1] :
-                return True
-            if i < len(board)-1 and board[i][j] == board[i+1][j]:
-                return True
-    return False
+import neat, random, os
+from game import *
 
- 
-            
-newgame()
-while(haspossiblemoves(board)):
-    boardsave = board
-    while(board == boardsave):
-        nextmove = random()
-        if nextmove < 0.4 :
-            board = updatedown(board)
-        elif nextmove < 0.8:
-            board = updateleft(board)
-        elif nextmove < 0.9:
-            board = updateright(board)
+
+def train_ai(genome, config):
+    net = neat.nn.FeedForwardNetwork.create(genome, config)
+    game = Game(3)
+    while game.haspossiblemoves():
+        output = net.activate((game.board[0][0],game.board[0][1],game.board[0][2],game.board[1][0],game.board[1][1],game.board[1][2],game.board[2][0],game.board[2][1],game.board[2][2]))
+        decision = output.index(max(output))
+        if decision == 0:
+            nextboard = game.updateright()
+        elif decision == 1:
+            nextboard = game.updateleft()
+        elif decision == 2:
+            nextboard = game.updatedown()   
         else:
-            board = updateup(board)
-    addnewtile()
-printboard()
+            nextboard = game.updateup()
+        if nextboard == game.board :
+            break
+        game.board = nextboard
+        game.addnewtile()
+    #for now, fitness is just going to be the sum of the values on the board.
+    total = 0
+    for line in game.board :
+        for cell in line:
+            total += cell
+    genome.fitness = total
+def eval_genomes(genomes, config):
+    for gen in genomes :
+        #These are tuples and we don't care about the first value
+        train_ai(gen[1], config)
+#In the config file, the number of inputs changes based on the length of the board
+def run_neat():
+    local_dir = os.path.dirname(__file__)
+    config_path = os.path.join(local_dir, "config.txt")
+    config = neat.Config(neat.DefaultGenome, neat.DefaultReproduction, neat.DefaultSpeciesSet, neat.DefaultStagnation,config_path)
 
+    p = neat.Population(config)
+    p.add_reporter(neat.StdOutReporter(True))
+    stats = neat.StatisticsReporter()
+    p.add_reporter(stats)
+    p.add_reporter(neat.Checkpointer(1))
 
-
-
-
-    
+    winner = p.run(eval_genomes, 50)
+    net = neat.nn.FeedForwardNetwork.create(winner, config)
+    game = Game(3)
+    while game.haspossiblemoves():
+        output = net.activate((game.board[0][0],game.board[0][1],game.board[0][2],game.board[1][0],game.board[1][1],game.board[1][2],game.board[2][0],game.board[2][1],game.board[2][2]))
+        decision = output.index(max(output))
+        if decision == 0:
+            nextboard = game.updateright()
+        elif decision == 1:
+            nextboard = game.updateleft()
+        elif decision == 2:
+            nextboard = game.updatedown()   
+        else:
+            nextboard = game.updateup()
+        if nextboard == game.board :
+            break
+        game.board = nextboard
+        game.addnewtile()
+        print(str(game))
+run_neat()
